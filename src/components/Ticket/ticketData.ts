c0920c02b711193e3280a8e3133cdbf40127bc1c
Pol Gubau Amores <gubaupol@gmail.com>
2025-06-11 19:38:38 +0200

    Refactor footer and project components for improved layout and styling
    Updated the footer component to enhance its styling and layout, ensuring better responsiveness. Adjusted the ProjectList and ProjectPreview components to improve the display of project items. Introduced a new ticketData module to streamline ticket item management and updated the Ticket component to utilize this data structure for rendering items dynamically.
    
    Issue: MW-0000
    Type: refactor

--- src/components/Ticket/ticketData.ts
+++ src/components/Ticket/ticketData.ts
@@ -2,65 +2,30 @@ export type TicketItem = {
   name: string;
   price: string | number;
 };
-/*
-<TicketItem name="Overpriced WordPress theme" price={99} />
-     
-      <li class="flex justify-between">
-        <span>Template that doesn't fit your brand</span>
-        <span>€199</span>
-      </li>
-      <li class="flex justify-between">
-        <span>SEO plugin that doesn't work</span>
-        <span>€49</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Opaque hosting provider</span>
-        <span>€79</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Plugin to make it work</span>
-        <span>€29</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Freelancer to fix everything</span>
-        <span>€299</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Other plugin to fix it (again)</span>
-        <span>€29</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Unclear maintenance costs</span>
-        <span>€49/month</span>
-      </li>
-      <li class="flex justify-between">
-        <span>Time lost</span>
-        <span>too much</span>
-      </li>
-*/
+
 type Ticket = TicketItem[]
 
-export const ticket: Ticket = [{
+export const ticketData: Ticket = [{
   name: "Overpriced WordPress theme",
-  price: 99
-}, {
-  name: "Template that doesn't fit your brand",
   price: 199
 }, {
-  name: "SEO plugin that doesn't work",
-  price: 49
+  name: "Template that doesn't fit your brand",
+  price: 159
+}, {
+  name: "SEO plugins that don't work",
+  price: 69
 }, {
   name: "Opaque hosting provider",
   price: 79
 }, {
   name: "Plugin to make it work",
-  price: 29
+  price: 39
 }, {
   name: "Freelancer to fix everything",
-  price: 299
+  price: 1599
 }, {
   name: "Other plugin to fix it (again)",
-  price: 29
+  price: 39
 }, {
   name: "Unclear maintenance costs",
   price: "49€/month"
